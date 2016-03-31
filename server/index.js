2179216d7fd3149fabe24771fda53f350ed8133a
Victor Koenders <victor.koenders@gmail.com>
2016-03-31 16:26:39 +0200

    Blueprint of what I want to do

---
+++ server/index.js
@@ -0,0 +1,25 @@
+// TODO: Watch directory with actions and auto-update
+// TODO: Add database connections
+// TODO: Add public directory
+// TODO: Add source directory and auto-build that to the public directory
+module.exports = {
+    start: function(options){
+/*
+options = {
+    react: {
+        source: './source',
+        output: './public/build.js'
+    },
+    database: {
+        type: 'mongodb',
+        url: '',
+        schemaDirectory: './schema'
+    },
+    server: {
+        directory: './public/',
+        default: './public/index.html'
+    }
+};
+ */
+    }
+};
